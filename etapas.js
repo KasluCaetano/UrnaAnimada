14fc56bc21db6df1ee414fd592f7f29b3cc2afd0
lucas <kaslucaetano@gmail.com>
2024-08-03 11:20:43 -0300

    first commit

---
+++ etapas.js
@@ -0,0 +1,68 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '11111',
+                nome: 'Sid',
+                partido: 'Movimento dos Birutas',
+                fotos:[
+                    {url:'sid.png', legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero: '22222',
+                nome: 'Coragem',
+                partido: 'Partido dos Coxinha',
+                fotos:[
+                    {url:'coragem.png', legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero: '33333',
+                nome: 'Lula Molusco',
+                partido: 'Aliança dos Preguiçosos',
+                fotos:[
+                    {url:'lulamoslusco.png', legenda: 'Vereador'}
+                ]
+            }
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '44',
+                nome: 'Kowalski',
+                partido: 'Movimento dos Birutas',
+                vice: 'Salsicha',
+                fotos:[
+                    {url:'kowalski.png', legenda: 'Prefeito'},
+                    {url:'salsicha.png', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero: '55',
+                nome: 'Scooby Doo',
+                partido: 'Partido dos Coxinha',
+                vice: 'Jojnny Bravo',
+                fotos:[
+                    {url:'scoobydoo.png', legenda: 'Prefeito'},
+                    {url:'johnnybravo.png', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero: '66',
+                nome: 'Burro',
+                partido: 'Aliança dos Preguiçosos',
+                vice: 'Dory',
+                fotos:[
+                    {url:'burro.png', legenda: 'Prefeito'},
+                    {url:'dory.png', legenda: 'Vice-Prefeito', small: true}
+                ]
+            }
+        ]
+    }
+];
